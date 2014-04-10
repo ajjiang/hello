a2c23c7be7bacfdf81801b13a641a7775bedd8ff
Andrew <ajjiang555@yahoo.com>
2014-04-10 15:44:01 -0700

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String argv[]){
 		//default is World
+		//Author: Andrew Jiang (ajjiang555@yahoo.com)
 		String name = "World";
 		if(argv.length != 0){
 			name = argv[0];
