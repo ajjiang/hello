b843aebdcf9e180dd79131c5d1b11b43ca2cdab9
Andrew <ajjiang555@yahoo.com>
2014-04-10 16:05:59 -0700

    Made interactive

--- lib/Hello.java
+++ lib/Hello.java
@@ -1,11 +1,13 @@
-public class Hello{
-	public static void main(String argv[]){
-		//default is World
-		//Author: Andrew Jiang (ajjiang555@yahoo.com)
-		String name = "World";
-		if(argv.length != 0){
-			name = argv[0];
-		}
-		System.out.println("Hello, " + name + "!");
-	}
+import java.util.Scanner;
+
+public class Hello
+{
+    public static void main( String argv[] )
+    {
+        System.out.println("What's your name");
+        Scanner in = new Scanner(System.in);
+        String my_name = in.nextLine();
+
+        System.out.println( "Hello, " + my_name + "!" );
+    }
 }
